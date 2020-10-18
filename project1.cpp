445cdcad81641e12e114ab045ba135827fd860d1
veltsonbastien <bastienveltson@outlook.com>
2020-10-18 12:02:52 -0700

    at this point it compiles, so i'm just pushing this version

--- project1.cpp
+++ project1.cpp
@@ -6,7 +6,9 @@
 
 #include "project1.h"
 #include <iostream>
-#include <utility> 
+#include <utility>
+#include <string>
+#include <cstring>
 
 using std::cout;
  
@@ -14,7 +16,7 @@ using std::cout;
 TST::TST() : root(0) { }
 
 // constructor with a word if you want 
-TST::TST(string word){
+TST::TST(char* word){
  root = new Node(word); 
 }
 
@@ -35,7 +37,7 @@ void TST::clear(Node *n) {
 
 
 //search main function
-void TST::lookup(string word){
+void TST::lookup(char* word) const{
   //handle when there is an empty tree
   if(!root){
    cout<<word<<" not found"<<endl;    
@@ -44,19 +46,19 @@ void TST::lookup(string word){
   }//end of else
 }//end of search
 
-void TST::lookup(string word, Node* n){
+void TST::lookup(char* word, Node* n) const{
     //this will basically be similar to insert, but instead of inserting, we show that it is found
     //check if a node is in DAMN existence 
     if(n!=NULL){
 	
     	//first check root and what it's got goin' on up there, the only n coming in here should be root's  
     	//but before we move on, check if it equals what's going on, and INTERVENE accordingly
-    	if( ( (strcmp(word, n->keyLeft.first))==0){
-        cout<<word<<"found, count="<<keyLeft.second<<endl;
+    	if( (strcmp(word, n->keyLeft.first))==0){
+        cout<<word<<"found, count="<<n->keyLeft.second<<endl;
 	}//if it's in the key left
         //or is it found in key right
-	else if(strcmp(word, n->keyRight.first))==0){
-       	cout<<word<<"found, count="<<keyRight.second<<endl; 
+	else if(strcmp(word, n->keyRight.first)==0){
+       	cout<<word<<"found, count="<<n->keyRight.second<<endl; 
     	}
 
         //first check left and right entries to see if it has a chance at even being in this node - if not, try in another place zibs
@@ -73,7 +75,7 @@ void TST::lookup(string word, Node* n){
     else{
      //if node doesn't exist, you know what to do
      //it's not here, so say that
-     count<<word<<" not found"<<endl;
+     cout<<word<<" not found"<<endl;
     } //end of else check for that boi
 
 
@@ -81,7 +83,7 @@ void TST::lookup(string word, Node* n){
 
 
 // insert main function
-void TST::insert(string word) {
+void TST::insert(char* word) {
     // handle special case of empty tree first
     if (!root) {
 	root = new Node(word);
@@ -95,27 +97,27 @@ void TST::insert(string word) {
 
 
 // recursive helper for insert (assumes n is never 0)
-void TST::insert(string word, Node* n) {
+void TST::insert(char* word, Node* n) {
 
     //check if a node is in DAMN existence 
     if(n!=NULL){
 	
     //first check root and what it's got goin' on up there, the only n coming in here should be root's  
     //but before we move on, check if it equals what's going on, and INTERVENE accordingly
-    if( (strcmp(word, n->keyLeft.first))==0) keyLeft.second++; 
-    else if((strcmp(word, n->keyRight.first))==0) keyRight.second++; 
+    if( (strcmp(word, n->keyLeft.first))==0) n->keyLeft.second++; 
+    else if((strcmp(word, n->keyRight.first))==0) n->keyRight.second++; 
 
 	//next check if there is a keyRight value 
 	//if there isn't handle it properly
 	if(n->keyRight.second == 0){
 	   //if the value is less than keyLeft, switch keyLeft and keyRight
 	   if( (strcmp(word, n->keyLeft.first))==-1 ){
-            string tempString = keyLeft.first;
-	    string tempCount = keyLeft.second;
-	    keyLeft.first = word; 
-	    keyLeft.second = 1; 
-	    keyRight.first = tempString;
-	    keyRight.second = tempCount; 
+            char* tempString = n->keyLeft.first;
+	    int tempCount = n->keyLeft.second;
+	    n->keyLeft.first = word; 
+	    n->keyLeft.second = 1; 
+	    n->keyRight.first = tempString;
+	    n->keyRight.second = tempCount; 
 	   }else{
            //put it in keyRight
 	   n->keyRight.first = word;
@@ -143,7 +145,7 @@ void TST::insert(string word, Node* n) {
 
 } //end of check
 
-
+/*
 pair<Node*, int> getNodeInfo(string word){
   //they are going to pass in a word, but we know we wanna compare with root
   getNodeInfo(word, root); 
@@ -246,179 +248,22 @@ pair<Node*, int> getNodeInfo(string word, Node* n){
     //else, return the info for the getSuccessor, which SHOULD have one of these 
     return getNodeInfo(getSuccessorWord(nodeInfo.first,nodeInfo.second)); 
 }//end of getNodeInfo
+*/ 
 
 
 //go through everything, but only print if it's between the range
-void TST::print(Node* n, string word1, string word2) const{
+void TST::print(Node* n, char* word1, char* word2) const{
   if(n!=NULL){
     if(n->left) print(n->left,word1,word2); 
-    if( (strcmp(n->keyLeft.first, word1))==1 && (strcmp(n->keyLeft.first, word2))=-1 ) cout<<n->keyLeft.first<<endl;
+    if( ( (strcmp(n->keyLeft.first, word1))==1 && (strcmp(n->keyLeft.first, word2))==-1 ) || (strcmp(n->keyLeft.first,word1))==0 || (strcmp(n->keyLeft.first,word2))==0 ) cout<<n->keyLeft.first<<endl;
     if(n->middle) print(n->middle,word1,word2);
-    if( (strcmp(n->keyRight.first, word1))==1 && (strcmp(n->keyRight.first, word2))=-1 ) cout<<n->keyRight.first<<endl;
+    if( ((strcmp(n->keyRight.first, word1))==1 && (strcmp(n->keyRight.first, word2))==-1) || (strcmp(n->keyRight.first,word1))==0 || (strcmp(n->keyRight.first,word2))==0 ) cout<<n->keyRight.first<<endl;
     if(n->right) print(n->right,word1,word2);
   }
 }
 
 //do a range search given two words 
-void TST::range_search(string word1, string word2){
+void TST::range_search(char* word1, char* word2){
   print(root,word1,word2); 
 }//end of rangeSearch
 
-
-// print tree data in-order, with helper
-void IntBST::printInOrder() const {
-    printInOrder(root);
-}
-void IntBST::printInOrder(Node *n) const {
-    //check the edge cases first 
-    if(n!=NULL){
-      if(n->left){
-        printInOrder(n->left); 
-      }//end of n-left
-      if(n->middle){
-        printInOrder(n->middle); 
-      }
-       cout<<n->info<<" "; 
-      if(n->right){
-        printInOrder(n->right); 
-      }//end of n-right
-    }//end of null check     
-}
-
-// IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
-// Parameters:
-// int value: the value to be found
-// Node* n: the node to start with (for a recursive call)
-// Whenever you call this method from somewhere else, pass it
-// the root node as "n"
-TST::Node* TST::getNodeFor(string word, Node* n) const{
-    //edge case 
-    if(n==NULL) return n; 
-    //if they're the same, return root 
-    else if(n->info == value) return n;
-    //check if we need to continue down the left side  
-    else if(value <= n->info) return getNodeFor(value, n->left); 
-    //or check if we need to continue down the right side
-    else return getNodeFor(value, n->right); 
-}
-
-// returns true if value is in the tree; false if not
-bool TST::contains(int value) const {
-    //edge case
-    if(getNodeFor(value, root)!=0){
-     return true; 
-    } 
-    return false; 
-}
-
-/*
-// returns the Node containing the predecessor of the given value
-TST::Node* TST::getPredecessorNode(string word) const{   
-	Node* n = getNodeFor(word, root); 
-	Node* temp = 0; 
-	if(n->left != 0){
-          temp = n->left; 
-	  while(temp->right != 0){
-            temp = temp->right; 
-	  }//end of while 
-	  return temp; 
-	}//end of if
-        else{
-         Node* temp2 = root;
-	 Node* n2 = getNodeFor(value, root); 
-	 while(temp2->info != n2->info){
-           if(n2->info >= temp2->info){
-              temp = temp2;
-	      temp2 = temp2->right; 
-	   }//end of inner if 
-           else{
-            temp2 = temp2->left; 
-	   }//end of else 
-	 }//end of while 
-	 return temp;
-	}	
-	return root; 
-}
- 
-
-// returns the predecessor 
-//value of the given value or 0 if there is none
-int IntBST::getPredecessor(int value) const{
-    if(getPredecessorNode(value) != NULL){
-      int valueToReturn = getPredecessorNode(value)->info;  
-      return valueToReturn; 
-    }else
-    { 
-    return 0;
-    }
-    return getPredecessorNode(value)->info; 
-}
-*/
-/*
-// returns the Node containing the successor of the given value
-IntBST::Node* IntBST::getSuccessorNode(int value) const{
-      Node* n = getNodeFor(value,root); 
-      Node* temp = 0; 
-      if(n->right != NULL){
-        temp = n->right; 
-	while(temp->left != NULL){
-          temp = temp->left; 
-	}//end of while 
-	return temp; 
-      } else{
-        Node* temp2 = root; 
-	Node* n2 = getNodeFor(value, root); //512 
-	while(temp2->info != n2->info){
-         if(n2->info <= temp2->info){
-	  temp = temp2; 
-	  temp2 = temp2->left; 
-	 }//end of if check 
-	  else{
-           temp2 = temp2->right;
-	  }
-	}//end of second while 
-         return temp; 	
-      }
-      return root;  
-}
-
-
-// returns the successor value of the given value or 0 if there is none
-int IntBST::getSuccessor(int value) const{
-    if(getSuccessorNode(value) != NULL){
-      int valueToReturn = getSuccessorNode(value)->info; 
-      return valueToReturn; 
-    } 
-    return 0; 
-}
-
-
-
-/*
-// DELETE: deletes the Node containing the given word from the tree
-// returns true if the node exists and was deleted or false if the node does not exist
-bool TST::remove(string word){
-   //check if it's a sole leaf 
-   if(root == NULL || getNodeFor(word,root) == NULL ) return false;
-   else{
-    if(root->left == NULL && root->right == NULL){
-       delete root;
-       root = NULL;  
-       return true;
-    }
-    else if(root->left == NULL){
-       Node* temp = root; 
-       root = root->right; 
-       delete temp;  
-       return true; 
-    } else if (root->right == NULL){
-       Node* temp = root;
-       root = root->left;
-       delete temp; 
-       return true; 
-    } else {
-      return true; 
-    } 
-   }
-   return false;
-   */ 
--- project1.h
+++ project1.h
@@ -15,34 +15,27 @@ class TST {
  public:
     // ctor, dtor, insert and one print method already done in project1.cpp:
     TST();                   // constructor
+    TST(char* word);        // constructor with a word
     ~TST();                  // destructor
 
     //Needed functions 
-    void lookup(string word) const; //searches for the string word in the TST 
-    void insert(string word); //inserts the string word into the TST
-    void remove(string word); //deletes the string word from the TST 
-    void range_search(string s1, string s2); //search for strings within this range
-
- 
+    void lookup(char* word) const; //searches for the string word in the TST
+    void insert(char* word); //inserts the string word into the TST
+    void remove(char* word); //deletes the string word from the TST 
+    void range_search(char* s1, char* s2); //search for strings within this range 
     //Other methods that would be nice to have 
-    void printInOrder() const;       // print tree data in-order to cout, will be helpful for range_search
-    bool contains(string word) const;  // true if word is in the TST, will be helpful for quickly checking if words are in the tree or no
-    //string getPredecessor(string word) const;       // returns the predecessor string of the given value or null if there is none
-    //string getSuccessor(string word) const;         // returns the successor string of the given value or null if there is none
-    
-
 
  private:
 
     struct Node {
-	pair <string,int> keyRight;
-	pair <string,int> keyLeft; 
+	pair <char*,int> keyRight;
+	pair <char*,int> keyLeft; 
 	Node *left, *right, *middle;
 
-        Node(string word){
-	  keyLeft.first=word;
+        Node(char* word){
+	  *(keyLeft.first)=*word;
 	  keyLeft.second = 1;
-	  keyRight.first ="";
+	  *(keyRight.first)='\0';
 	  keyRight.second = 0; 
 	  left = 0; 
 	  middle = 0; 
@@ -54,11 +47,11 @@ class TST {
     Node *root;
 
     // recursive utility functions for use by public methods above
-    Node* getNodeFor(string word, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
-    void clear(Node *n); // for destructor
-    void insert(string word, Node *n); // note overloading names for simplicity
-    void printInOrder(Node *n) const;
-
+    void clear(Node* n); 
+    void insert(char* word, Node *n); // note overloading names for simplicity
+    void print(Node *n) const;
+    void lookup(char* word, Node* n) const; //helper for that
+    void print(Node* n, char* word1, char* word2) const;       // print tree data in-order to cout, will be helpful for range_search  
     // these should be used by getPredecessor and getSuccessor, and ONE of them should be used by remove
     //Node* getSuccessorNode(string word) const;   // returns the Node containing the successor of the given value
     //Node* getPredecessorNode(string word) const; // returns the Node containing the predecessor of the given value 
